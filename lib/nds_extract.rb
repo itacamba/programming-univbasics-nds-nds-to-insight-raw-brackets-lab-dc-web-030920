83e6f3937fbfd5f8ac26cdf71ed6e49c8c9fa75c
Carla D Sahagun <daniela.sahagun@hotmail.com>
2020-02-13 03:52:58 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,21 +2,18 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
-  # Remember, it's always OK to pretty print what you get *in* to make sure
-  # that you know what you're starting with!
-  #
-  #
-  # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
-  result = {
-  }
-  #
-  # Use loops, variables and the accessing method, [], to loop through the NDS
-  # and total up all the
-  # ...
-  # ...
-  # ...
-  #
-  #
-  # Be sure to return the result at the end!
-  nil
+  total_gross_from_movies = 0
+  directors_counter = 0
+  my_hash = {}
+  while directors_counter < directors_database.length do
+    movie_counter = 0
+    director_name = directors_database[directors_counter][:name]
+    while movie_counter < directors_database[directors_counter][:movies].length do
+      total_gross_from_movies += directors_database[directors_counter][:movies][movie_counter][:worldwide_gross]
+      movie_counter += 1
+    end
+    my_hash[director_name] = total_gross_from_movies # This adds the new key and new value to the hash.
+    directors_counter += 1
+  end
+    p my_hash
 end
