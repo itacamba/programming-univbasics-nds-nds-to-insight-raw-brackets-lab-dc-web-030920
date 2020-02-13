642263facc4f38f886ab7c448fdae4c5dda1adaa
Carla D Sahagun <daniela.sahagun@hotmail.com>
2020-02-13 03:56:45 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -13,6 +13,7 @@ def directors_totals(nds)
       movie_counter += 1
     end
     my_hash[director_name] = total_gross_from_movies # This adds the new key and new value to the hash.
+    total_gross_from_movies = 0
     directors_counter += 1
   end
     p my_hash
